16f5820eca0c4d2d1b68ae7af19e1ec91f2cd95e
Harshit Batra <78447359+harshitbatra-25@users.noreply.github.com>
2021-09-05 17:36:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Audio-Genre-Classifier
+
+Audio Classifier
+Classify Between : Rock & Hip-Hop
+
+https://audio-based-genre-classifier.herokuapp.com/
---
+++ app.py
@@ -0,0 +1,35 @@
+import pickle
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+Filename = "model/model_tree.pkl"
+
+with open(Filename, 'rb') as file:
+    model = pickle.load(file)
+
+
+@app.route('/')
+def index_page():
+    print(model)
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict_logic():
+
+    if request.method == 'POST':
+        acousticness = float(request.form.get('acousticness'))
+        danceability = float(request.form.get('danceability'))
+        energy = float(request.form.get('energy'))
+        instrumentalness = float(request.form.get('instrumentalness'))
+        liveness = float(request.form.get('liveness'))
+        speechiness = float(request.form.get('speechiness'))
+        tempo = float(request.form.get('tempo'))
+        valence = float(request.form.get('valence'))
+    pred_name = model.predict([[acousticness, danceability, energy, instrumentalness, liveness, speechiness, tempo, valence]]).tolist()[0]
+    return render_template('index.html', pred_name=pred_name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
